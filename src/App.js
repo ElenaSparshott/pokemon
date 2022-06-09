0d7372adc66e166c4e722a3dee1fdc4391374b25
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-06-09 20:18:40 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import { useState } from 'react';
 
 function App() {
+
+  const [allPokemon, setAllPokemon] = useState([])
+
   return (
     <div className="app-container">
       <h1>Pokemon codex</h1>
+      <div className="Pokemon-container">
+        <div className="all-container">
+
+        </div>
+        <button className="Press-me">Press Me!</button>
+      </div>
     </div>
   );
 }

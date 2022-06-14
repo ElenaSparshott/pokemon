f28684821438df459d6cb665a7efb16813dde320
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-06-14 21:33:26 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { useState } from 'react';
 function App() {
 
   const [allPokemon, setAllPokemon] = useState([])
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+  const getAllPokemon = async () => {
+    const res = await fetch
+  } 
 
   return (
     <div className="app-container">
       <h1>Pokemon codex</h1>
       <div className="Pokemon-container">
         <div className="all-container">
-
         </div>
         <button className="Press-me">Press Me!</button>
       </div>

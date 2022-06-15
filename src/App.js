92616ba3618e780e4f1a00c5e9d3e7231a63eeb9
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-06-15 17:30:40 +0100

    pokemon cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import PokemonCards from './components/PokemonCards';
 
 function App() {
 
-  const [allPokemon, setAllPokemon] = useState([])
+  const [allPokemons, setAllPokemons] = useState([])
   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
 
-  const getAllPokemon = async () => {
-    const res = await fetch
-  } 
+  const getAllPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
+
+    setLoadMore(data.next)
+
+    function createPokemonObject(result) {
+      result.forEach( async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json()
+
+        setAllPokemons(currentlist => [...currentlist, data])
+      })
+    }
+      createPokemonObject(data.results)
+      await console.log(allPokemons)
+    }
+  
+
+  useEffect(() => {
+   getAllPokemons()
+  }, [])
+  
 
   return (
     <div className="app-container">
       <h1>Pokemon codex</h1>
       <div className="Pokemon-container">
         <div className="all-container">
+          {allPokemons.map(pokemon => <li>{pokemon.name}</li>)}
         </div>
         <button className="Press-me">Press Me!</button>
       </div>
     </div>
   );
-}
+  }
 
 export default App;
---
+++ src/components/PokemonCards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PokemonCards = (id, name, image, type) => {
+  return (
+    <div className='card'>
+        <div className='number'>
+            <small>#0{id}</small>
+        </div>
+        <img src={image} alt={name}/>
+        <div className='details'>
+            <h3>{name}</h3>
+            <small>Type: {type}</small>
+        </div>
+
+    </div>
+  )
+}
+
+export default PokemonCards
